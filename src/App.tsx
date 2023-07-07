f873c811f90ab83d2186f7bf1dc07f5e8565c02f
Muhammad Kashif <mk.kashif.se@gmail.com>
2023-07-07 20:15:05 +0500

    type fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,38 @@
 import "./App.css";
-import useRecipe from "./hooks/useRecipe";
 import AppContext, { AppProvider } from "./hooks/AppContext";
+import IngredientList from "./components/IngredientList";
+import RecipeCard from "./components/RecipeCard";
 import { useContext } from "react";
 
-function IngredientMenu(props: any) {
-  const { filteredIngredients, filterIngredients, searchRecipes } = useContext(AppContext);
-
-  const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const { value } = e.target as HTMLInputElement;
-    filterIngredients!(value);
-  };
-
-  return (
-    <div className="flex flex-col space-y-2">
-      <h1 className="text-2xl font-bold">Ingredients</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="border w-full p-2 rounded-lg"
-          onKeyUp={(e) => onKeyUp(e)}
-        />
-      </div>
-      <div className="flex-1 overflow-scroll cursor-pointer">
-        {filteredIngredients!.map((ingredient) => (
-          <div
-            onClick={() => searchRecipes!(ingredient.strIngredient)}
-            className="p-2 rounded-lg px-2 space-x-4 border-b border-gray-100 hover:bg-blue-100 flex  items-center"
-            key={ingredient.idIngredient}
-          >
-            <div>
-              <img
-                className="w-8 rounded"
-                src={ingredient.thumbnail}
-                alt={ingredient.thumbnail}
-              ></img>
-            </div>
-            <div>{ingredient.strIngredient}</div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
 function App() {
-  const { searchRecipes } = useRecipe();
+  const { recipes, selectedIngredient } = useContext(AppContext);
 
   return (
     <AppProvider>
       <main className="flex space-x-2">
-        <div>
-          <IngredientMenu />
+        <div className="w-1/5 px-4 flex flex-col">
+          <IngredientList />
         </div>
-        <div>RIGHT</div>
+        <div className="w-4/5 px-4 border-l overflow-scroll">
+          <div className="justify-between mb-2 -ml-4 fixed bg-white px-4 border p-2 rounded-r-xl shadow-xl">
+            <h2 className="text-gray-700 font-bold text-lg">
+              Recipes with {selectedIngredient?.strIngredient}
+            </h2>
+            <div className="text-gray-500 text-sm">
+              {" "}
+              Total Number of Recipes {recipes?.length}{" "}
+            </div>
+          </div>
+          <div className="grid grid-cols-4 gap-8 mt-20">
+            {recipes?.map((recipe) => (
+              <RecipeCard
+                onViewRecipe={(recipe) => console.log(recipe)}
+                key={recipe.idMeal}
+                recipe={recipe}
+              />
+            ))}
+          </div>
+        </div>
       </main>
     </AppProvider>
   );
